56d99665e07358ec8792aa4c675569281f35d10d
DylanEtris <dylanletris@gmail.com>
2021-04-08 16:27:25 -0400

    Updated binary file changes

--- DecodeHackRF.m
+++ DecodeHackRF.m
@@ -6,15 +6,15 @@ n = 256;
 y = linspace(x1,x2,n);
 
 fileID = fopen(filePath);
-data = fread (fileID, [sampleNum, 2], 'uint8');
+data = fread (fileID, [sampleNum, 2], 'float');
 realData = data(:, 1);
 complexData = data(:, 2);
-try
-    for k = 1:length(realData)
-        realData(k) = y(realData(k) + 1);
-        complexData(k) = y(complexData(k) + 1);
-    end
-catch ME
-    fprintf("%s", ME.message)
-end
+% try
+%     for k = 1:length(realData)
+%         realData(k) = y(realData(k) + 1);
+%         complexData(k) = y(complexData(k) + 1);
+%     end
+% catch ME
+%     fprintf("%s", ME.message)
+% end
 end
